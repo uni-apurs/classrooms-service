d7e2b046c7f4975143a5ad71a5434de01e2211ea
Stefan Katic <katicstefan.coder@gmail.com>
2021-07-23 09:49:59 +0200

    first commit

---
+++ src/main/java/com/apurs/microservices/classroomsservice/ClassroomsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apurs.microservices.classroomsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apurs/microservices/classroomsservice/ClassroomsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apurs.microservices.classroomsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
